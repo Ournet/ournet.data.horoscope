933b2cd9f01ad87bf0f8497c11c22150354d2a03
Dumitru Cantea <canteadumitru@gmail.com>
2017-01-15 23:14:32 +0200

    hash atonic text

--- test/utils.js
+++ test/utils.js
@@ -8,7 +8,7 @@ describe('utils', function() {
 		const r1 = utils.randomInt();
 		const r2 = utils.randomInt();
 
-		console.log(r1, r2);
+		// console.log(r1, r2);
 
 		assert.equal(false, r1 === r2);
 	});
